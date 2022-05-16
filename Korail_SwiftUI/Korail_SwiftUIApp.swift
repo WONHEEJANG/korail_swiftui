f49b74c5c10b731f3473b096ae66eeee9ddf984c
WONHEEJANG <jangwh93@gmail.com>
2022-05-16 21:08:17 +0900

    Initial Commit

---
+++ Korail_SwiftUI/Korail_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Korail_SwiftUIApp.swift
+//  Korail_SwiftUI
+//
+//  Created by Jason on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct Korail_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
